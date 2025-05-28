034b14850d3dc5dfb1be40e6faa1384e6eea6bc1
ynsun <m15668931031@163.com>
2025-05-28 15:37:57 +0800

    spring ai

---
+++ demo3/spring-ai-pro/src/main/java/com/example/springaipro/SpringAiProApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiProApplication.class, args);
+    }
+
+}
